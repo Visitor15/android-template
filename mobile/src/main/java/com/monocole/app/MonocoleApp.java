1d1a3ef9fbda767a66eea201db8b4469015b8bad
Nick Champagne <nchampagne@sharecare.com>
2016-10-25 13:24:25 -0400

    Added LeakCanary dependency.

--- mobile/src/main/java/com/monocole/app/MonocoleApp.java
+++ mobile/src/main/java/com/monocole/app/MonocoleApp.java
@@ -1,9 +1,12 @@
 package com.monocole.app;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.monocole.app.dagger.BaseActivityComponent;
 import com.monocole.app.dagger.DaggerBaseActivityComponent;
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 /**
  * Created by visitor15 on 10/18/16.
@@ -13,11 +16,18 @@ public class MonocoleApp extends Application {
 
     private static MonocoleApp singleton;
 
+    private static RefWatcher refWatcher;
+
     private static BaseActivityComponent baseActivityComponent;
 
     @Override
     public void onCreate() {
         MonocoleApp.singleton = this;
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
         init();
         super.onCreate();
     }
@@ -26,8 +36,13 @@ public class MonocoleApp extends Application {
         return MonocoleApp.singleton;
     }
 
+    public static RefWatcher getRefWatcher() {
+        return MonocoleApp.singleton.refWatcher;
+    }
+
     private void init() {
-        baseActivityComponent = DaggerBaseActivityComponent.builder().build();
+        refWatcher              = LeakCanary.install(MonocoleApp.singleton);
+        baseActivityComponent   = DaggerBaseActivityComponent.builder().build();
     }
 
     public BaseActivityComponent getSimpleActivityComponent() {
